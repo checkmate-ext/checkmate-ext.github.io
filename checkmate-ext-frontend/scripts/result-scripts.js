242b2c971f5c27f344881c392faf41273fdf5398
Ayberk Berat Eroğlu <ayberkberateroglu@gmail.com>
2024-12-19 21:14:52 +0300

    fix script bug

---
+++ checkmate-ext-frontend/scripts/result-scripts.js
@@ -0,0 +1,8 @@
+    // Button actions (placeholders)
+    document.getElementById('moreDetailsBtn').addEventListener('click', () => {
+        alert('More details functionality goes here.');
+    });
+
+    document.getElementById('reportMistakeBtn').addEventListener('click', () => {
+        alert('Report mistake functionality goes here.');
+    });
