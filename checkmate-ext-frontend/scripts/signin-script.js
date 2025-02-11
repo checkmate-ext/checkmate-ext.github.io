33ea273cb9334900d254220ddc5d2e1dba33ef16
ba2 <alpbatuaksans@gmail.com>
2025-02-11 18:17:03 +0300

    added loading animation to sign in button

--- checkmate-ext-frontend/scripts/signin-script.js
+++ checkmate-ext-frontend/scripts/signin-script.js
@@ -2,20 +2,29 @@ import AuthService from '../authentication/auth-service.js';
 
 const authService = new AuthService('http://localhost:5000');
 
-document.getElementById('signInForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', () => {
+    const signInButton = document.getElementById('signInToMain');
 
-    try {
-        const result = await authService.login(email, password);
-        if (result.success) {
-            // Optionally alert token or log it if needed
-            alert('Signed in successfully! with token: ' + authService.token);
-            // navigate to main menu
-            navigateTo('MainMenuPage.html');
+    const originalSignInButtonText = signInButton.innerHTML;
+
+    document.getElementById('signInForm').addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        signInButton.innerHTML = '<div class="loading-spinner"></div>';
+
+        try {
+            const result = await authService.login(email, password);
+            if (result.success) {
+                // Optionally alert token or log it if needed
+                alert('Signed in successfully! with token: ' + authService.token);
+                // navigate to main menu
+                navigateTo('MainMenuPage.html');
+            }
+        } catch (error) {
+            signInButton.innerHTML = originalSignInButtonText;
+            alert(error.message || 'An error occurred during sign-in');
+
         }
-    } catch (error) {
-        alert(error.message || 'An error occurred during sign-in');
-    }
+    });
 });
