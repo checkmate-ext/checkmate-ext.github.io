cda5a7e9f66a727980372e85c8de7454d2988534
ba2 <alpbatuaksans@gmail.com>
2025-04-27 15:29:33 +0300

    fixed client id

---
+++ checkmate-ext-frontend/background.js
@@ -0,0 +1,17 @@
+// Clear any cached tokens when the extension starts
+chrome.runtime.onInstalled.addListener(() => {
+    clearCachedTokens();
+});
+
+function clearCachedTokens() {
+    chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
+        if (token) {
+            console.log("Found cached token, removing it...");
+            chrome.identity.removeCachedAuthToken({ 'token': token }, function() {
+                console.log("Token removal completed");
+            });
+        } else {
+            console.log("No cached token found");
+        }
+    });
+}
