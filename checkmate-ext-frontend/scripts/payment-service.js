4014ce2c7fdcd6fc2ec2347d44604586cf9c247f
AyberkEroglu <ayberkberateroglu@gmail.com>
2025-05-01 14:19:37 +0300

    backend compatibility and google fix

--- checkmate-ext-frontend/scripts/payment-service.js
+++ checkmate-ext-frontend/scripts/payment-service.js
@@ -1,5 +1,5 @@
 // payment-service.js
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://checkmate-backend-api-1029076451566.us-central1.run.app';
 
 class PaymentService {
     constructor() {
