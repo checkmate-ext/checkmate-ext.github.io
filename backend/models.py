a49e8790222ce2f525c9e84567a3ce19e4bdb8df
ba2 <alpbatuaksans@gmail.com>
2025-05-01 18:16:59 +0300

    fixed the indentation 🗿🗿

--- backend/models.py
+++ backend/models.py
@@ -20,10 +20,10 @@ class User(db.Model):
     iyzico_customer_id = db.Column(db.String(128), nullable=True)
 
 
-def set_password(self, password):
-    self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-def check_password(self, password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
 
